3116948f32cd5aafa569245839a859458b2bfe93
TommyWang <tommycx1999@gmail.com>
2022-05-16 19:03:15 -0400

    added files

---
+++ src/main/frontend/simple_image_upload/src/App.js
@@ -0,0 +1,69 @@
+import './App.css';
+import axios from "axios";
+import React, { useState, useEffect, useCallback } from "react";
+import { useDropzone } from 'react-dropzone'
+
+const UserProfiles = () => {
+  const [userProfiles, setUserProfiles] = useState([]);
+  const fetchUserProfiles = () => {
+    axios.get("http://localhost:8080/api-v1/userProfile").then(res => {
+      console.log(res);
+      setUserProfiles(res.data);
+
+    })
+  }
+  useEffect(() => {
+    fetchUserProfiles();
+  }, []);
+  return userProfiles.map((user, index) => {
+    return (<div className="card" key={index}>
+      {user.id ? <img className="image" src={`http://localhost:8080/api-v1/userProfile/${user.id}/image/download`}></img> : null}
+      <br />
+
+      <h1>{user.userName}</h1>
+      <MyDropzone id={user.id} />
+      <br />
+
+    </div>)
+
+  })
+}
+
+function MyDropzone({ id }) {
+  const onDrop = useCallback(acceptedFiles => {
+    const file = acceptedFiles[0];
+    console.log(file);
+    const formData = new FormData();
+    formData.append("file", file);
+    axios.post(`http://localhost:8080/api-v1/userProfile/${id}/image/upload`,
+      formData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data"
+        }
+      }).then(() => {
+        window.location.reload(false);
+      }).catch(err => {
+        console.log(err)
+      })
+  }, [])
+  const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
+
+  return (
+    <div {...getRootProps()} className="image-area">
+      <input {...getInputProps()} />
+        <p>Drag, drop or click to upload a new image</p>
+    </div>
+  )
+}
+
+
+function App() {
+  return (
+    <div className="App">
+      <UserProfiles />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/test/java/com/example/onlineimagewarehouse/OnlineImageWarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.onlineimagewarehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineImageWarehouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
